2b1f0f90807a327342fae36a4f52e1c0a58bfaf1
nguyenhung101196 <dunggoilaanh@gmail.com>
2018-03-14 01:34:54 +0700

    thêm form pro detail, user infor

---
+++ NERA_WEB_APP/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NERA_WEB_APP.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult product()
+        {
+            return View();
+        }
+
+
+        public ActionResult details_product()
+        {
+            return View();
+        }
+    }
+}
