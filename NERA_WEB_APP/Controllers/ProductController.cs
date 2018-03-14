29fd95b70b67aba3854488cbedaf5044e54b94c8
nguyenhung101196 <dunggoilaanh@gmail.com>
2018-03-14 13:07:05 +0700

    view

---
+++ NERA_WEB_APP/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NERA_WEB_APP.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult product()
+        {
+            return View();
+        }
+
+        public ActionResult details()
+        {
+            return View();
+        }
+    }
+}
---
+++ NERA_WEB_APP/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NERA_WEB_APP.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult user_infor()
+        {
+            return View();
+        }
+    }
+}
