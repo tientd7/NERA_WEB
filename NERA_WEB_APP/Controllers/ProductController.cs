302b4015a8923469861b7169c9ce8152794a4439
Hieu Mai Duy <algidsnow@outlook.com>
2018-03-19 01:25:43 +0700

    thêm sản phẩm + Hiển thị sản phẩm dịch vụ

--- NERA_WEB_APP/Controllers/ProductController.cs
+++ NERA_WEB_APP/Controllers/ProductController.cs
@@ -16,6 +16,8 @@ namespace NERA_WEB_APP.Controllers
             return View();
         }
 
+
+        //lấy danh sách sản phẩm có trong bảng MenuItem 
         public JsonResult showSP()
         {
             var show = from i in db.CS_Menu_Item where i.Item_Type == "SP" select i;
@@ -26,12 +28,39 @@ namespace NERA_WEB_APP.Controllers
         {
             return View();
         }
-
+        
         public ActionResult addNewProduct()
         {
             return View();
         }
 
+        [HttpPost]
+        public JsonResult addNewProduct(CS_Menu_Item Obj)
+        {
+            CS_Menu_Item newObj = new CS_Menu_Item();
+            int id = new App_Auto_NumberController().GenID("CS_Menu_Item.Item_Id");
+            newObj.Item_Id = id;
+            newObj.Item_Name = Obj.Item_Name;
+            newObj.Enable = Obj.Enable;   
+            newObj.Item_Type = Obj.Item_Type;
+            newObj.Meta_Desc = Obj.Meta_Desc;
+            newObj.Meta_Key = Obj.Meta_Key;
+            newObj.Language = Obj.Language;
+            db.CS_Menu_Item.Add(newObj);
+            db.SaveChanges();
+            return Json(newObj);
+        }
+
+
+
+    
+
+        //lấy danh sách Dịch vụ có trong bảng MenuItem 
+        public JsonResult showDV()
+        {
+            var show = from i in db.CS_Menu_Item where i.Item_Type == "DV" select i;
+            return Json(show, JsonRequestBehavior.AllowGet);
+        }
 
 
 
--- NERA_WEB_APP/Scripts/Controller/Product/product.js
+++ NERA_WEB_APP/Scripts/Controller/Product/product.js
@@ -1,15 +1,94 @@
 ﻿app.controller("ProductCtrl", function ($scope, $http) {
-    $scope.Item;
+
+    // tạo biến item sản phẩm
+    $scope.ItemSP;
+
+    //tạo biến item dv
+    $scope.ItemDV;
+
+    // hàm gọi lấy danh sách sản phẩm
     $scope.showSP = function () {
-        $http.get('/Product/showSP').success(function (data,status) {
-            $scope.Item = data;
-            console.log("success" + data+ status);
+        $http.get('/Product/showSP').success(function (data, status) {
+            $scope.ItemSP = data;
+            console.log("success" + data + status);
             for (var i = 0; i < $scope.Item.length; i++) {
                 console.log(i);
             }
         }).error(function (error) {
             console.log("error :" + error);
         })
-    }
+    };
+    // chạy hàm
     $scope.showSP();
+
+    // hàm gọi lấy danh sách dich vụ
+    $scope.showDV = function () {
+        $http.get('/Product/showDV').success(function (data, status) {
+            $scope.ItemDV = data;
+            console.log("success" + data + status);
+            
+        }).error(function (error) {
+            console.log("error :" + error);
+        })
+    };
+    // chạy hàm
+    $scope.showDV();
+
+
+    $scope.Item;
+    $scope.Create = function () {
+
+        $http({
+            method: 'POST',
+            url: '/Product/addNewProduct',
+            data: $scope.Item
+        }).success(function (data, status, headers, config) {
+            if (data != "") {
+                console.log("Thêm Thành Công");
+                window.location.href = '/Product/product';
+
+            }
+            else {
+                console.log('Form data not Saved!');
+
+            }
+        }).error(function (data, status, headers, config) {
+            $scope.message = 'Unexpected Error while saving data!!' + data.errors;
+            console.log($scope.message);
+        });
+
+    }
 })
+
+
+app.controller('addNewCtr', function ($scope, $http) {
+    $scope.message = "";
+    $scope.listProduct;
+
+
+    $scope.Create = function () {
+
+        $http({
+            method: 'POST',
+            url: '/Product/addNewProduct',
+            data: $scope.Item
+        }).success(function (data, status, headers, config) {
+            if (data != "") {
+                console.log("Thêm Thành Công");
+                window.location.href = '/Product/product';
+
+            }
+            else {
+                console.log('Form data not Saved!');
+
+            }
+        }).error(function (data, status, headers, config) {
+            $scope.message = 'Unexpected Error while saving data!!' + data.errors;
+            console.log($scope.message);
+        });
+
+    }
+
+});
+
+//
