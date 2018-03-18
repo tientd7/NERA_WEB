b753cdd91df326bc8906d86a62842bcb89491042
nguyenhung101196 <dunggoilaanh@gmail.com>
2018-03-19 02:09:28 +0700

    view bảng giá

---
+++ NERA_WEB_APP/Controllers/PriceController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NERA_WEB_APP.Controllers
+{
+    public class PriceController : Controller
+    {
+        // GET: Price
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult listPrice()
+        {
+            return View();
+        }
+
+        public ActionResult addNewListPrice()
+        {
+            return View();
+        }
+    }
+}
