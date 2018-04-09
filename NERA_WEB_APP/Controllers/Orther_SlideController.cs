dbf4e83a4a779ea4104c548abc1cddcc7a5e5533
Tien Tran <tranduytien1302@gmail.com>
2018-04-09 23:02:18 +0700

    Demo form thêm sửa xóa

--- NERA_WEB_APP/Controllers/Orther_SlideController.cs
+++ NERA_WEB_APP/Controllers/Orther_SlideController.cs
@@ -23,5 +23,48 @@ namespace NERA_WEB_APP.Controllers
             return Json(List, JsonRequestBehavior.AllowGet);
 
         }
+
+        public CS_Other_Slide getById(int Id)
+        {
+            return db.CS_Other_Slide.Find(Id);
+        }
+        [HttpPost]
+        public JsonResult Insert(CS_Other_Slide item)
+        {
+            CS_Other_Slide obj = new CS_Other_Slide();
+            obj.Tbl_Id = new App_Auto_NumberController().GenID("CS_Other_Slide.Tbl_Id");
+            obj.Slide_Type = item.Slide_Type;
+            obj.Language = item.Language;
+            obj.Image_URL = item.Image_URL;
+            obj.Image_Title = item.Image_Title;
+            obj.Image_Order = item.Image_Order;
+            obj.Image_Link = item.Image_Link;
+            obj.Enable = item.Enable;
+            db.CS_Other_Slide.Add(obj);
+            db.SaveChanges();
+
+            return Json("OK");
+        }
+        [HttpPost]
+        public JsonResult Update(CS_Other_Slide Slide)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(Slide);
+                db.SaveChanges();
+                return Json("OK");
+            }else
+                return Json("ModelState Invalid!");
+        }
+
+        [HttpPost]
+        public JsonResult Delete(int Delete_Id)
+        {
+            var obj = getById(Delete_Id);
+            db.CS_Other_Slide.Remove(obj);
+            db.SaveChanges();
+            return Json("OK");
+        }
+
     }
 }
--- NERA_WEB_APP/Scripts/Controller/Orther_Slide/Orther_Slide.js
+++ NERA_WEB_APP/Scripts/Controller/Orther_Slide/Orther_Slide.js
@@ -1,14 +1,85 @@
 ﻿app.controller("Orther_Slide", function ($scope, $http) {
 
     $scope.List = "";
+    this.IsEditing = false;
+    this.EditId = -1;
+    getAllDataSlide();
 
-    $http.get('/Orther_Slide/GetData/')
-        .success(function (data) {
-            $scope.List = data; 
-        })
-        .error(function (error) {
-            console.log(error)
-        })
+    function clearAllControl() {
+        $scope.CreateItem = {};
+    }
 
-    
-})
+    $scope.onCreateSlide = function () {
+        var obj = {
+            Tbl_Id: 0,
+            Image_Title: $scope.CreateItem.Image_Title,
+            Image_URL: $scope.CreateItem.Image_URL,
+            Image_Link: $scope.CreateItem.Image_Link,
+            Image_Orde: $scope.CreateItem.Image_Orde,
+            Enable: $scope.CreateItem.Enable,
+            Slide_Type: $scope.CreateItem.Slide_Type,
+            Language: $scope.CreateItem.Language
+        };
+        var config = {
+            data: obj
+        };
+        $http.post('/Orther_Slide/Insert', config).then(function (data) {
+            getAllDataSlide();
+        },function (error) {
+            console.log(error);
+        });
+        clearAllControl();
+    };
+
+    $scope.onEditSlide = function (id) {
+        this.EditId = id;
+        this.IsEditing = true;
+    };
+
+    $scope.onCancelSlide = function () {
+        this.IsEditing = false;
+    };
+
+    $scope.onDeleteSlide = function (id) {
+        var config = {
+            data: { Delete_Id: id }
+        };
+        $http.post('/Orther_Slide/Delete', config).success(function (data) { getAllDataSlide(); }).error(function (error) { console.log(error); });
+    };
+
+    $scope.onSaveSlide = function (item) {
+        //var obj = {
+        //    Tbl_Id: item.Tbl_Id,
+        //    Image_Title: item.Image_Title,
+        //    Image_URL: item.Image_URL,
+        //    Image_Link: item.Image_Link,
+        //    Image_Orde: item.Image_Orde,
+        //    Enable: item.Enable,
+        //    Slide_Type: item.Slide_Type,
+        //    Language: item.Language
+        //};
+        var config = {
+            params: { Slide: item }
+        };
+        $http.post('/Orther_Slide/Update', config).then(function (data, status, headers, config) { getAllDataSlide(); },function (error) { console.log(error); });
+    };
+
+    $scope.getStatusSlide = function (id) {
+        if (this.IsEditing)
+            if (this.EditId === id)
+                return false;
+        return true;
+    };
+    function getAllDataSlide() {
+        $http.get('/Orther_Slide/GetData')
+            .success(function (data) {
+                $scope.ListSlide = data;
+            })
+            .error(function (error) {
+                console.log(error)
+            });
+    };
+
+
+
+});
