c86821f1a5d0bbe40364794d1e209fef1d1fcada
Hieu Mai Duy <algidsnow@outlook.com>
2018-04-16 00:58:48 +0700

    new slide

---
+++ NERA_WEB_APP/Controllers/Post_SlideNewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NERA_WEB_APP.Controllers
+{
+    public class Post_SlideNewController : Controller
+    {
+        // GET: Post_SlideNew
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
