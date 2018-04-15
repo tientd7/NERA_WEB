ccc17dd44e8d811bb94cb982c54faae34ef943e7
Tien Tran <tranduytien1302@gmail.com>
2018-04-15 22:15:18 +0700

    ckeditor

--- NERA_WEB_APP/Scripts/Controller/BaiViet/ckeditor/config.js
+++ NERA_WEB_APP/Scripts/Controller/BaiViet/ckeditor/config.js
@@ -10,10 +10,10 @@ CKEDITOR.editorConfig = function (config) {
     //config.shiftEnterMode = CKEDITOR.ENTER_P
     //config.autoParagraph = false; // stops automatic insertion of <p> on focus
     config.filebrowserBrowseUrl = '../Scripts/Controller/BaiViet/ckfinder/ckfinder.html?type=Files';
-    config.filebrowserImageBrowseUrl = '../Scripts/Controller/BaiViet/ckfinder/ckfinder.html?Type=Images';
-    config.filebrowserFlashBrowseUrl = '../Scripts/Controller/BaiViet/ckfinder/ckfinder.html?Type=Flash';
-    config.filebrowserUploadUrl = '../Scripts/Controller/BaiViet/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Files';
-    config.filebrowserImageUploadUrl = '../Scripts/Controller/BaiViet/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Images';
-    config.filebrowserFlashUploadUrl = '../Scripts/Controller/BaiViet/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Flash';
+    config.filebrowserImageBrowseUrl = '/Scripts/Controller/BaiViet/ckfinder/ckfinder.html?Type=Images';
+    config.filebrowserFlashBrowseUrl = '/Scripts/Controller/BaiViet/ckfinder/ckfinder.html?Type=Flash';
+    config.filebrowserUploadUrl = '/Scripts/Controller/BaiViet/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Files';
+    config.filebrowserImageUploadUrl = '/Scripts/Controller/BaiViet/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Images';
+    config.filebrowserFlashUploadUrl = '/Scripts/Controller/BaiViet/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Flash';
     //CKFinder.setupCKEDitor(null, 'Scripts/Controller/BaiViet/ckfinder/')
 };
